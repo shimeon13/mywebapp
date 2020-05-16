fcf90289d82d8189382990cc10c39ef81c74e454
Teru <shimeon.0103@gmail.com>
2020-05-17 01:39:40 +0900

    Update settings.py

--- weight_webapp/settings.py
+++ weight_webapp/settings.py
@@ -137,6 +137,10 @@ try:
 except ImportError:
     pass
 
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {'default':dj_database_url.config()}
+
 DATABASES['default'].update(db_from_env)
+
 STATIC_URL = '/static/'
