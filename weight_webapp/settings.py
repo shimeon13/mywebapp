3b5cc58ad6761c87f748caa6a9a8bed8bbecd386
Teru <shimeon.0103@gmail.com>
2020-05-17 22:28:54 +0900

    weight

--- weight_webapp/settings.py
+++ weight_webapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -127,8 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS =(
-    os.path.join(BASE_DIR, 'static')
-)
+    os.path.join(BASE_DIR, 'static'),
+    )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
